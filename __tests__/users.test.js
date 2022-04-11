2d2eb65d2ca4ea65073de65634b442ca4255859d
ckaynatividad <natividad.ckay@gmail.com>
2022-04-11 10:38:06 -0700

    redirect uri passing

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/utils/github');
+
 describe('gotchi-clone routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +12,13 @@ describe('gotchi-clone routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should redirect to github oauth', async () => {
+    const req = await request(app).get('/api/v1/users/login');
+
+    expect(req.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+    );
   });
 });
