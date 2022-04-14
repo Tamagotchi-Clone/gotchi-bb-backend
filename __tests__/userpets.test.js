dd6c3df4ac8a594245bab537ecb2b0ca7f63702c
Violet Stewart <vkazoo2@gmail.com>
2022-04-14 12:49:34 -0700

    sign up via post passing:

--- __tests__/userpets.test.js
+++ __tests__/userpets.test.js
@@ -3,8 +3,6 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-jest.mock('../lib/utils/github');
-
 describe('gotchi-clone routes', () => {
   beforeEach(() => {
     return setup(pool);
--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -3,9 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-jest.mock('../lib/utils/github');
-
-describe('gotchi-clone routes', () => {
+describe('gotchi-clone auth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -14,26 +12,10 @@ describe('gotchi-clone routes', () => {
     pool.end();
   });
 
-  it('should redirect to github oauth', async () => {
-    const req = await request(app).get('/api/v1/users/login');
-
-    expect(req.header.location).toMatch(
-      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
-    );
-  });
-
-  it('should login and redirect users to to profile', async () => {
-    const req = await request
-      .agent(app)
-      .get('/api/v1/users/login/callback?code=42')
-      .redirects(1);
-
-    expect(req.body).toEqual({
-      id: expect.any(String),
-      email: 'not-real@example.com',
-      username: 'Admin',
-      iat: expect.any(Number),
-      exp: expect.any(Number),
-    });
+  it('signs a user up via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/users')
+      .send({ username: 'violet', password: 'gotchi is cool' });
+    expect(res.body).toEqual({ id: expect.any(String), username: 'violet' });
   });
 });
